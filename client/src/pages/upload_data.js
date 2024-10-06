100edbf8f7d8358119ba51e28c60c42a23e0f096
Rajsekar Balaji <brj3raj@gmail.com>
2024-10-07 00:54:08 +0530

    Added table render functionality

--- client/src/pages/upload_data.js
+++ client/src/pages/upload_data.js
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import DataTable from "../functions/DataTable";
 
 function FileUpload() {
     let [file, setFile] = useState(null);
+    let [df, setDf] = useState(null);
+    let [showTable, setShowTable] = useState(false);
+
 
     const handleFileChange = (e) => {
         if(e.target.files[0]) {
@@ -24,17 +28,46 @@ function FileUpload() {
             method: 'POST',
             body: formData
         })
-        console.log(formData);
+        // fetch("/data")
+        // .then((res) => res.json())
+        // .then((data) => {
+        //     console.log(data);
+        // })
     }
 
+    const handlePrint = (e) => {
+        //e.preventDefault();
+
+        fetch("http://localhost:9000/data")
+        .then((res) => res.json())
+        .then((data) => {
+            let dataList = JSON.parse(data);
+            setDf(dataList);
+            console.log(typeof(dataList));
+
+            console.log("Fetched Data: ", dataList);
+            setShowTable(true);
+        })
+        
+        console.log(`Dataframe is ${df}`)
+
+    }
+    
+
     return (
-        <form onSubmit={handleUpload}>
-            <div>
-                <input type="file" onChange={handleFileChange} />
-            </div>
-            <button type="submit">Upload</button>
-        </form>
-    )
+        <div>
+            <form onSubmit={handleUpload}>
+                <div>
+                    <input type="file" onChange={handleFileChange} />
+                </div>
+                <button type="submit">Upload</button>
+            </form>
+
+            <input type="button" value={showTable ? "Hide Table": "Show Table"} placeholder="Print" onClick={handlePrint} />
+            {showTable && <DataTable data={df} />}
+        </div>
+    );
+
 }
 
 
